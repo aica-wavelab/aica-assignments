66b6597105e7ac2d8a8b75c7c08bc6dd8133e2a4
BZoennchen <benedikt.zoennchen@web.de>
2025-04-08 15:22:03 +0200

    update wrong pip install.

--- A2_image_classification_of_museum_artefacts/tests/q1.py
+++ A2_image_classification_of_museum_artefacts/tests/q1.py
@@ -10,5 +10,5 @@ def test_loss(env):
     assert 'loss' in env, 'loss is not defined.'
     assert 'accuracy' in env, 'accuracy is not defined.'
     assert env['loss'] < 5.0, 'loss should be at least smaller than 5.0'
-    assert env['accuracy'] > 0.6, 'accuracy should be at least greater than 0.6'
+    assert env['accuracy'] > 0.65, 'accuracy should be at least greater than 0.65'
 
